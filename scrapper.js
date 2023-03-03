c85ea8bd2b3d62ab9aa522a57a4f6bcfa151c0b4
Terrance Tran <t.tranterrance@gmail.com>
2023-03-02 19:29:41 -0800

    Add functionality to scrapper

--- scrapper.js
+++ scrapper.js
@@ -1,2 +1,21 @@
 //File to contain Amazon webpage scrapper
 //window.location.href to grab current url
+
+const puppeteer = require('puppeteer');
+
+async function scrapeProduct(url){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const [el] = await page.$x('//*[@id="productTitle"]/text()');
+    const txt = await el.getProperty('textContent');
+    const rawTxt = await txt.jsonValue();
+
+    console.log(rawTxt);
+
+    browser.close();
+}
+
+// scrapeProduct(window.location.href);
+scrapeProduct("https://www.amazon.com/MATBEBY-Mattress-Breathable-Stretches-Protector/dp/B08KNZL6BB/ref=sr_1_5?crid=21QIA02SPE4JQ&keywords=mattress%2Btopper&qid=1677813441&sprefix=%2Caps%2C174&sr=8-5&th=1");
